bce6754ac8818cce66a049801e530b75ec785e79
Christina Li <cli240@student.monash.edu>
2021-04-30 18:44:22 +1000

    initial commit

---
+++ hey m8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hey m8
+//
+//  Created by Christina Li on 30/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
