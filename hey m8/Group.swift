89299952a9c80dda4a4d49ea259ab8bd4c17bf9f
Christina Li <cli240@student.monash.edu>
2021-05-23 23:24:14 +1000

    group.swfit din't get added up until now somehow

---
+++ hey m8/Group.swift
@@ -0,0 +1,23 @@
+//
+//  Group.swift
+//  hey m8
+//
+//  Created by Christina Li on 23/5/21.
+//
+
+import Foundation
+
+class Group: NSObject {
+    var name: String
+    var groupID: String
+    var members: Array<String>
+    
+    init(name: String, groupID: String, members: Array<String>) {
+        self.name = name
+        self.groupID = groupID
+        self.members = members
+    }
+    
+    
+}
+
